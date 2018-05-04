fb3c704cbc2e8c3abd85a7b6ef00ca7477b28342
Abdülhamit Gencal <abdulhamit_gfb@hotmail.com>
2018-05-04 14:45:25 +0300

    appjs

--- app.js
+++ app.js
@@ -56,8 +56,6 @@ app.post("/success", function (req, res){
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  var soru1text = document.getElementById("soru1text");
-  soru1text = soruArr[0];
   console.log("server starting on " + appEnv.url);
 });
 
